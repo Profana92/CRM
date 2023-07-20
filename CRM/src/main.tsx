ac21e916f3128b3015e110f963485811911c38bc
Profanum <stevevtek@gmail.com>
2023-07-20 11:01:32 +0200

    feat(router children added): router children added

--- CRM/src/main.tsx
+++ CRM/src/main.tsx
@@ -4,11 +4,28 @@ import App from './App.tsx';
 import './index.css';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Root from 'routes/Root.tsx';
+import ErrorPage from 'routes/pages/ErrorPage.tsx';
+
+const router = createBrowserRouter([
+   {
+      path: '/',
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      children: [
+         {
+            path: 'Children',
+            element: <h1>Children</h1>,
+         },
+      ],
+   },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
    <React.StrictMode>
       <Provider store={store}>
-         <App />
+         <RouterProvider router={router} />
       </Provider>
    </React.StrictMode>,
 );
