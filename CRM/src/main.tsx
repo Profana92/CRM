d6fd2d8ad46a57ad09878ac7cca16ede5e3f8e0e
Profanum <stevevtek@gmail.com>
2023-07-20 14:19:46 +0200

    feat(clock): added clock

--- CRM/src/main.tsx
+++ CRM/src/main.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
 import './index.css';
 import { store } from './app/store.js';
 import { Provider } from 'react-redux';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from 'routes/Root.tsx';
 import ErrorPage from 'routes/pages/ErrorPage.tsx';
+import Home from 'routes/pages/Home.tsx';
 
 const router = createBrowserRouter([
    {
@@ -15,7 +15,11 @@ const router = createBrowserRouter([
       errorElement: <ErrorPage />,
       children: [
          {
-            path: 'Children',
+            index: true,
+            element: <Home />,
+         },
+         {
+            path: 'children',
             element: <h1>Children</h1>,
          },
       ],
--- CRM/src/routes/pages/Home.tsx
+++ CRM/src/routes/pages/Home.tsx
@@ -1,7 +1,36 @@
-import React from 'react';
-
+import { useEffect } from 'react';
+import { Counter } from 'features/counter/Counter';
+import { Title } from 'features/currentPageTitle';
+import { useSelector, useDispatch } from 'react-redux';
+import { setPageTitle } from 'features/currentPageTitleSlice';
+import type { RootState } from 'app/store';
 function home() {
-   return <div>home</div>;
+   const count = useSelector((state: RootState) => state.pageTitle.value);
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+      document.title = 'Home';
+      dispatch(setPageTitle('Home'));
+   }, []);
+
+   return (
+      <>
+         <div className="">
+            <Counter />
+            <div>
+               <div>
+                  <button aria-label="Increment value" onClick={() => dispatch(setPageTitle('Dziala'))}>
+                     Increment
+                  </button>
+                  <span>{count}</span>
+                  <button aria-label="Decrement value" onClick={() => dispatch(setPageTitle('Tez dziala'))}>
+                     Decrement
+                  </button>
+               </div>
+            </div>
+         </div>
+      </>
+   );
 }
 
 export default home;
