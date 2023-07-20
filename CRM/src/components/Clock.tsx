16365dc5e691a1ff74c163fa4e05c16de8b1a649
Profanum <stevevtek@gmail.com>
2023-07-20 15:16:52 +0200

    feat(navbar): navbar styles

--- CRM/src/components/Clock.tsx
+++ CRM/src/components/Clock.tsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from 'react';
 function Clock() {
-   const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
+   const [currentTime, setCurrentTime] = useState(new Date());
    useEffect(() => {
       const interval = setInterval(() => {
-         setCurrentTime(new Date().toLocaleTimeString());
+         setCurrentTime(new Date());
       }, 1000);
       return () => clearInterval(interval);
    }, []);
 
-   return <p>{currentTime}</p>;
+   return (
+      <p className="hidden lg:block text-2xl font-medium">{`${currentTime.getHours()} : ${currentTime.getMinutes()}  : ${
+         currentTime.getSeconds() < 10 ? '0' : ''
+      }${currentTime.getSeconds()}`}</p>
+   );
 }
 
 export default Clock;
